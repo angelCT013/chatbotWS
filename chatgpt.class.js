64790b0fb55f45f534e34ac319d698ff1062d97b
legna013 <legnaarmstrong@gmail.com>
2023-11-29 23:20:07 -0800

    modelo

--- chatgpt.class.js
+++ chatgpt.class.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 class ChatGPTClass {
   queue = [];
-  optionsGPT ={ model:"gpt-4-1106-preview"};
-  // optionsGPT ={ model:"gpt-3.5-turbo"};
+  // optionsGPT ={ model:"gpt-4-1106-preview"};
+  optionsGPT ={ model:"gpt-3.5-turbo"};
   openai = undefined;
 
   constructor(){
